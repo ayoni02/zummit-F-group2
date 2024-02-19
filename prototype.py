296b5ed439cad9aaa3fecb95085571e9c3f89724
ayoni02 <105247084+ayoni02@users.noreply.github.com>
2024-02-19 21:36:48 +0100

    edits

--- prototype.py
+++ prototype.py
@@ -5,7 +5,7 @@ def install(package):
     subprocess.check_call([sys.executable, "-m", "pip", "install", "transformers"])
 
 #The above is just to install the transformers package if it is not already installed
-
+# In case of errors, comment it all out and use "pip install transformers" to install the package install
 
 
 from transformers  import pipeline
