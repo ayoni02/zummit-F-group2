ed57ee3c405e76880fbd6895dcc8f99b2935149d
ayoni02 <105247084+ayoni02@users.noreply.github.com>
2024-02-20 03:31:13 +0100

    f

--- app.py
+++ app.py
@@ -2,12 +2,7 @@ from transformers  import pipeline
 import streamlit as st
 
 
-classifier = pipeline("zero-shot-classification", model="facebook/bart-large-mnli")
 
-def classify(text):    
-    labels = ["POSITIVE", "NEGATIVE", "NEUTRAL"]
-    run_pipe = classifier(text, labels)
-    return run_pipe
 
 def main():
     text = st.text_input("Enter the text to classify: ") #input("Enter the text to classify: ")
