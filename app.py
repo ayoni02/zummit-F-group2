018380f674c4406ac4777d06072c7e1bdb832fc7
ayoni02 <105247084+ayoni02@users.noreply.github.com>
2024-02-20 03:44:21 +0100

    dfghj

--- app.py
+++ app.py
@@ -4,11 +4,7 @@ import streamlit as st
 
 classifier = pipeline("zero-shot-classification", model="facebook/bart-large-mnli")
 
-def classify(text):    
-    labels = ["POSITIVE", "NEGATIVE", "NEUTRAL"]
-    if text:
-        run_pipe = classifier(text, labels)
-        return run_pipe
+
 
 def main():
     text = st.text_input("Enter the text to classify: ") #input("Enter the text to classify: ")
