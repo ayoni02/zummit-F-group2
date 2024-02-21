7de77db7130244189cf72fb188668227a07c7cfe
ayoni02 <105247084+ayoni02@users.noreply.github.com>
2024-02-21 22:19:18 +0100

    qqq

--- app.py
+++ app.py
@@ -8,8 +8,9 @@ st.write(res)
 
 def main():
     text = st.text_input("Enter the text to classify: ") #input("Enter the text to classify: ")
+    labels = ["POSITIVE", "NEGATIVE", "NEUTRAL"]
     if text:
-        #results = classify(text)
+        #results = classifier(text)
         st.write("great")
     else:
         st.write("Please enter some text to classify")
