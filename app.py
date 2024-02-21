350dd6d0b44597dd1afb099ac2ec2dd791d37f8f
ayoni02 <105247084+ayoni02@users.noreply.github.com>
2024-02-21 21:25:51 +0100

    done exit

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ def main():
     text = st.text_input("Enter the text to classify: ") #input("Enter the text to classify: ")
     if text:
         #results = classify(text)
-        st.write()
+        st.write("great")
     else:
         st.write("Please enter some text to classify")
     
