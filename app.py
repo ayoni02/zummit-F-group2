0fdb6d27961a140568eb5620099f71a898d19a06
ayoni02 <105247084+ayoni02@users.noreply.github.com>
2024-02-22 16:41:45 +0100

    tttttttttttttttttttttttttttttttttt

--- app.py
+++ app.py
@@ -3,15 +3,21 @@ import streamlit as st
 #from functions import classify
 
 st.write("welcome")
-classifier = pipeline("zero-shot-classification", model="facebook/bart-large-mnli")
+#classifier = pipeline("zero-shot-classification", model="facebook/bart-large-mnli")
 #res = classify("i am a good boy")
 #st.write(res)
+
+@st.cache_data
+def rata():
+    # Fetch data from URL here, and then clean it up.
+    classifier = pipeline("zero-shot-classification", model="facebook/bart-large-mnli")
+    return classifier
 
 def main():
     text = st.text_input("Enter the text to classify: ") #input("Enter the text to classify: ")
     #labels = ["POSITIVE", "NEGATIVE", "NEUTRAL"]
     if text:
-        results = classifier(text)
+        results = rata(text)
         st.write(results['sequence'])
         st.write(results['labels'])
         st.write(results['scores'])
