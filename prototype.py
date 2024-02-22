accbb48afcacd83681549beff5627c93ac07367b
Jovackbud <victorkola97@gmail.com>
2024-02-22 23:56:32 +0100

    change labels dictionary and added suggestions for naming the result variable in puter scope

--- prototype.py
+++ prototype.py
@@ -1,23 +1,28 @@
 import subprocess
 import sys
 
+
 def install(package):
     subprocess.check_call([sys.executable, "-m", "pip", "install", "transformers"])
 
-#The above is just to install the transformers package if it is not already installed
+
+# The above is just to install the transformers package if it is not already installed
 # In case of errors, comment it all out and use "pip install transformers" to install the package install
 
 
-from transformers  import pipeline
+from transformers import pipeline
+
 
 def classify(text):
     classifier = pipeline("zero-shot-classification", model="facebook/bart-large-mnli")
-    labels = ["POSITIVE", "NEGATIVE", "NEUTRAL"]
+    labels = ["TOXIC", "AGGRESSIVE", "HATE SPEECH", 'INSULTIVE', 'SPAM', 'NSFW', 'SEXUAL']
 
     result = classifier(text, labels)
     return result
 
+
 if __name__ == "__main__":
     text = input("Enter the text to classify: ")
-    result = classify(text)
-    print(result)
+    result = classify(text) # we can change this so the variable name to maybe tag so it is not mixed up in future with the one in the classify function
+    print(
+        f'This content is {result}, please remove it and be careful going forward')  # print a statement instead of just tage
