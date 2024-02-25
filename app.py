e037b753e656ffd3c67d22bd4c2ad3da470ec999
ayoni02 <105247084+ayoni02@users.noreply.github.com>
2024-02-25 22:03:35 +0100

    made edits to corrolate it with the hugging face file

--- app.py
+++ app.py
@@ -2,26 +2,28 @@ from transformers  import pipeline
 import streamlit as st
 #from functions import classify
 
-st.write("welcome")
-#classifier = pipeline("zero-shot-classification", model="facebook/bart-large-mnli")
-#res = classify("i am a good boy")
-#st.write(res)
+st.write("Comment Moderation Model for Toxicity DetectionTask")
+st.write("""
+        Description:
+        In response to the growing concern over the proliferation of toxic comments within online communities, particularly within the context of the Zummit Community Learning platform, we aim to develop a sophisticated model capable of effectively identifying and flagging toxic comments. The proliferation of toxic comments, characterized by harmful language, offensive content, and disruptive behavior, presents a significant challenge to fostering a positive and inclusive online environment.
+        As such, this project seeks to leverage cutting-edge machine learning techniques and natural language processing (NLP) algorithms to analyze user-generated content and automatically detect instances of toxicity.
+        The primary objective of this initiative is to enhance community engagement and promote constructive discourse by proactively identifying and addressing toxic behavior within the Zummit platform. Build an advanced comment moderation model, that will mitigate the adverse effects of toxic comments and uphold the community's standards of respect, civility, and inclusivity.
+        """)
+classifier = pipeline("zero-shot-classification", model="facebook/bart-large-mnli")
 
-@st.cache_data
-def rata(txt):
-    # Fetch data from URL here, and then clean it up.
-    classifier = pipeline("zero-shot-classification", model="facebook/bart-large-mnli")
-    return classifier
 
 def main():
     text = st.text_input("Enter the text to classify: ") #input("Enter the text to classify: ")
-    #labels = ["POSITIVE", "NEGATIVE", "NEUTRAL"]
+    labels = ["TOXIC", "AGGRESSIVE", "HATE SPEECH", "INSULTIVE", "SPAM", "NSFW", "NEUTRAL"]
     if text:
-        results = rata(text)
-        st.write(results['sequence'])
-        st.write(results['labels'])
-        st.write(results['scores'])
+        results = classifier(text, labels)
+        #st.write(results['sequence'])
+        st.write(results['labels'][:3])
+        st.write(results['scores'][:3])
+        if results['labels'][0] != "NEUTRAL":
+            st.write(f"This post is considered {results['labels'][0]}, Please review you words")
     else:
         st.write("Please enter some text to classify")
+    st.write("")
     
 main()
